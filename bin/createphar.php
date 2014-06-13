b6aaa74845fcd2e4ab8a517161ea126ae2d12e5b
Jordi Boggiano <j.boggiano@seld.be>
2014-06-13 17:57:46 +0200

    Clean up pickle.php and phar output

--- bin/createphar.php
+++ bin/createphar.php
@@ -12,19 +12,34 @@ $p->compressFiles(Phar::GZ);
 $p->setSignatureAlgorithm (Phar::SHA1);
 
 $files = array();
-$files['stub.php']='pickle.php';
 
 $rd = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($src_root, FilesystemIterator::SKIP_DOTS));
 foreach ($rd as $file) {
     $path = basename($file->getPathInfo());
-    if (!($path == '.git'|| $path == 'tests' || $path == 'Tests')) {
+    if (!($path == '.git'|| $path == 'tests' || $path == 'Tests' || $path == 'bin')) {
         $files[substr($file->getPath() .'/' . $file->getFilename(), $base_dir_pos)] = $file->getPath(). '/' . $file->getFilename();
     }
 }
 
 $p->startBuffering();
 $p->buildFromIterator(new ArrayIterator($files));
+
+$content = file_get_contents(__DIR__.'/pickle');
+$content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+$p->addFromString('bin/pickle', $content);
+
+$stub = <<<'EOF'
+#!/usr/bin/env php
+<?php
+
+Phar::mapPhar('pickle.phar');
+
+require 'phar://pickle.phar/bin/pickle';
+
+__HALT_COMPILER();
+EOF;
+
+$p->setStub($stub);
 $p->stopBuffering();
 
-$p->setStub($p->createDefaultStub('pickle.php'));
 $p = null;
