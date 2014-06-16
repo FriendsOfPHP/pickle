ddf618e8d6ad0d6d7e93e94b4f31fb2073fde97a
Pierre Joye <pierre.php@gmail.com>
2014-06-16 15:46:30 +0200

    fix default for enable options being false by default

--- src/Console/Command/InstallerCommand.php
+++ src/Console/Command/InstallerCommand.php
@@ -32,40 +32,28 @@ class InstallerCommand extends Command
 
         $pkg = new Package($path);
         $options = $pkg->getConfigureOptions();
-        print_r($options);
         if ($options) {
             $options_value = [];
             $helper = $this->getHelperSet()->get('question');
 
             foreach ($options['enable'] as $name => $opt) {
-            print_r($opt);
             /* enable/with-<extname> */
             if ($name == $pkg->getName()) {
                 $options_value[$name] = true;
                 continue;
             }
-                switch ($opt->default) {
-                    case 'y':
-                        $default = true;
-                        break;
-                    case 'n':
-                        $default = false;
-                        break;
-                    default:
-                        $default = $opt->default;
-                        break;
-                }
-                $prompt = new ConfirmationQuestion($opt->prompt . " (default: " .$opt->default. "): ", $default);
-                $options_value['enable'][$name] = (object) ['type' => $opt->type, 'input' => $helper->ask($input, $output, $prompt)];
+            $default = $opt->default;
+            $prompt = new ConfirmationQuestion($opt->prompt . " (default: " .$opt->default. "): ", $default);
+            $options_value['enable'][$name] = (object) ['type' => $opt->type, 'input' => $helper->ask($input, $output, $prompt)];
             }
         }
 
-	$build = new BuildSrcUnix($pkg, $options_value);
-	$build->phpize();
-	$build->createTempDir();
-	$build->configure();
-	$build->install();
-	$build->cleanup();
+        $build = new BuildSrcUnix($pkg, $options_value);
+        $build->phpize();
+        $build->createTempDir();
+        $build->configure();
+        $build->install();
+        $build->cleanup();
 
     }
 }
