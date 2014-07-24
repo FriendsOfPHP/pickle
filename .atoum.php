8db94f216f31d58160605fc1f29e28bd7ea107c6
Anatol Belski <ab@php.net>
2014-07-24 18:07:54 +0200

    made the visibility ext to be known to atoum

--- .atoum.php
+++ .atoum.php
@@ -1,5 +1,8 @@
 <?php
 
+use mageekguy\atoum\visibility;
+
 $script->addTestsFromDirectory(__DIR__ . '/tests/units');
 $script->noCodeCoverageForNamespaces('Composer');
 $runner->addExtension(new \Atoum\PraspelExtension\Manifest());
+$runner->addExtension(new visibility\extension($script));
