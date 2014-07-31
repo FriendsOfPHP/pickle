3bf60f443816a5d8f2fd217a185e3cbe0777e927
Anatol Belski <ab@php.net>
2014-07-31 11:09:16 +0200

    added the constructor to the interface

--- src/Package/Convey/Command/Command.php
+++ src/Package/Convey/Command/Command.php
@@ -2,10 +2,12 @@
 
 namespace Pickle\Package\Convey\Command;
 
+use Composer\IO\ConsoleIO;
+
 interface Command
 {
+    public function __construct($path, ConsoleIO $io);
     public function execute($target, $no_convert);
-
     public function getType();
 }
 
