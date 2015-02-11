4a8cc02527b6ba9fa74f657612a0a3b7fb8cf7a4
Michael Wallner <mike@php.net>
2015-02-11 22:15:36 +0100

    fix exclusion check
    pickle release wanted to add the while git directory

--- src/GitIgnore.php
+++ src/GitIgnore.php
@@ -9,7 +9,11 @@ class GitIgnore
     {
         $dir = $package->getSourceDir();
         $path = $package->getSourceDir() . '/.gitignore';
-        $this->excluded = glob("$dir/.git/*");
+        $this->excluded = [
+            "$dir/.git/", "$dir/.gitignore", "$dir/.gitmodules",
+            "$dir/.svn/",
+            "$dir/.hg/"
+        ];
 
         if (is_file($path) === false) {
             throw new \InvalidArgumentException('File not found: ' . $path);
@@ -51,9 +55,11 @@ class GitIgnore
 
     public function isExcluded(\SplFileInfo $file)
     {
-        return (
-            in_array(basename($file), ['.gitignore', '.gitmodules']) ||
-            in_array($file->getRealPath(), $this->excluded)
-        );
+        foreach ($this->excluded as $path) {
+            if (!strncmp($file,  $path, strlen($path))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
