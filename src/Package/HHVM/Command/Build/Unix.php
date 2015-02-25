75f43148eb294f0f9339311157b0d019dc865dd3
Anatol Belski <ab@php.net>
2015-02-25 11:16:45 -0800

    basic steps for the HHVM ext compilation process

---
+++ src/Package/HHVM/Command/Build/Unix.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Pickle\Package\HHVM\Command\Build;
+
+use Pickle\Base\Abstracts;
+use Pickle\Base\Interfaces;
+
+class Unix extends Abstracts\Package\Build implements Interfaces\Package\Build
+{
+    public function prepare()
+    {
+	    $this->hphpize();
+    }
+
+    public function hphpize()
+    {
+        $backCwd = getcwd();
+        chdir($this->pkg->getSourceDir());
+
+        $res = $this->runCommand('phpize');
+        chdir($backCwd);
+        if (!$res) {
+            throw new \Exception('phpize failed');
+        }
+    }
+
+    public function configure($opts = null)
+    {
+        $backCwd = getcwd();
+        chdir($this->pkg->getSourceDir());
+
+        $res = $this->runCommand('cmake .');
+        chdir($backCwd);
+        if (!$res) {
+            throw new \Exception('cmake failed');
+        }
+    }
+
+    public function make()
+    {
+        $backCwd = getcwd();
+        chdir($this->tempDir);
+        $res = $this->runCommand('make');
+        chdir($backCwd);
+
+        if (!$res) {
+            throw new \Exception('make failed');
+        }
+    }
+
+    public function install()
+    {
+        $backCwd = getcwd();
+        chdir($this->tempDir);
+        $res = $this->runCommand('make install');
+        chdir($backCwd);
+        if (!$res) {
+            throw new \Exception('make install failed');
+        }
+    }
+}
