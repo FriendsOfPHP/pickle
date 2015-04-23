185dfac0f80041a26834e4a17c86c4dcc5f8ed75
Anatol Belski <ab@php.net>
2015-04-23 11:26:45 +0200

    missing include and restore the needed var

--- src/Package/PHP/Convey/Command/DefaultExecutor.php
+++ src/Package/PHP/Convey/Command/DefaultExecutor.php
@@ -5,6 +5,7 @@ namespace Pickle\Package\PHP\Convey\Command;
 use Pickle\Base\Interfaces;
 use Pickle\Base\Abstracts\Package\Convey;
 use Pickle\Package\PHP;
+use Pickle\Package\PHP\Util\PackageXml;
 
 class DefaultExecutor implements Interfaces\Package\Convey\DefaultExecutor
 {
@@ -15,6 +16,7 @@ class DefaultExecutor implements Interfaces\Package\Convey\DefaultExecutor
     public function execute($target, $no_convert)
     {
         $jsonLoader = new \Pickle\Package\Util\JSON\Loader(new \Pickle\Package\Util\Loader());
+        $pickle_json = $target . DIRECTORY_SEPARATOR . "composer.json";
         $package = null;
 
         if (file_exists($pickle_json)) {
