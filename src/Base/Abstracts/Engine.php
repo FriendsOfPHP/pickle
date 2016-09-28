c8b17c91dd6199f08cec5a320a93acca3aac2aad
Ankit Pokhrel <ankitpokhrel@gmail.com>
2016-09-28 23:12:40 +0700

    Refactor getArchitecture

--- src/Base/Abstracts/Engine.php
+++ src/Base/Abstracts/Engine.php
@@ -42,21 +42,13 @@ abstract class Engine
     {
         /* Just a basic method, a concrete Engine implementation
         might need to override this. */
-        $is_64_bit = 8 == PHP_INT_SIZE;
+        $is64Bit = 8 == PHP_INT_SIZE;
 
         if (defined('PHP_WINDOWS_VERSION_MAJOR')) {
-            if ($is_64_bit) {
-                return 'x64';
-            } else {
-                return 'x86';
-            }
-        } else {
-            if ($is_64_bit) {
-                return 'x86_64';
-            } else {
-                return 'i386';
-            }
+            return $is64Bit ? 'x64' : 'x86';
         }
+
+        return $is64Bit ? 'x86_64' : 'i386';
     }
 }
 
