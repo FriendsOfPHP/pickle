18cfb2f9fabe0c4cbe8248d16192f69dbc799f02
Christoph M. Becker <cmbecker69@gmx.de>
2020-05-25 15:14:14 +0200

    Check existence and executability of unescaped filename
    `is_file()` and `is_executable()` can't handle shell escaped filenames,
    what is a particular problem on Windows; e.g. `C:\php\php.exe` is shell
    escaped as `C:^\php^\php.exe`.

--- src/Engine/PHP.php
+++ src/Engine/PHP.php
@@ -58,11 +58,10 @@ class PHP extends Abstracts\Engine implements Interfaces\Engine
 
     public function __construct($phpCli = PHP_BINARY)
     {
-        $phpCli = escapeshellcmd($phpCli);
         if (!(is_file($phpCli) && is_executable($phpCli))) {
             throw new \Exception("Invalid php executable: $phpCli");
         }
-        $this->phpCli = $phpCli;
+        $this->phpCli = escapeshellcmd($phpCli);
         $this->getFromConstants();
     }
 
